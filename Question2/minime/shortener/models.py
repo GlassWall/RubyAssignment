6dccaf58be8861e0b046c91df092cd4741e5cc52
Karthik Ravi <karthik.ravi@treebohotels.com>
2018-09-02 22:41:12 +0530

    renamed file

--- Question2/minime/shortener/models.py
+++ Question2/minime/shortener/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class miniURL(models.Model):
     url = models.CharField(max_length=300, )
     shortcode = models.CharField(max_length=20, unique=True)
+    updated = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return str(self.url)
 
